201e2b7fb0950536e3b2d5bce4b7005a9a5dd778
Harsh Srivastava <hsrivastava@splunk.com>
2022-05-23 10:57:28 +0530

    Made minor changes in the app

--- koodous_consts.py
+++ koodous_consts.py
@@ -1,6 +1,6 @@
 # File: koodous_consts.py
 #
-# Copyright (c) 2018-2021 Splunk Inc.
+# Copyright (c) 2018-2022 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
